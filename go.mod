bf407d6e6ef9d8068d9070ee841e440941a682ca
Michał Sidor <public+git@meekchopp.es>
2021-09-25 13:25:18 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/michcioperz/michci.ooo-daemon
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+)
+
+var validProjects *map[string]string = nil
+
+func panicIfErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func loadConfig() (config *map[string]string) {
+	config = new(map[string]string)
+	f, err := os.Open("repository-secrets.json")
+	panicIfErr(err)
+	defer panicIfErr(f.Close())
+	dec := json.NewDecoder(f)
+	panicIfErr(dec.Decode(config))
+	return
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	repo := r.URL.User.Username()
+	token, _ := r.URL.User.Password()
+	validToken, repoExists := (*validProjects)[repo]
+	if !repoExists || (token != validToken) {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	cmd := exec.Command("./build_project.sh", repo)
+	cmd.Stdout = w
+	log.Printf("run of %v finished: %v", repo, cmd.Run())
+}
+
+func main() {
+	go func() {
+		for {
+			validProjects = loadConfig()
+			time.Sleep(60)
+		}
+	}()
+	for validProjects == nil {
+	}
+	http.HandleFunc("/", handle)
+	log.Panic(http.ListenAndServe(":31400", nil))
+}
